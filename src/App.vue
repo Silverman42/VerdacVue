d74ed8e25d099d177207220b98bc50ff8bcf2a6e
Sylvester Nkeze <snkeze9@gmail.com>
2024-09-06 19:36:23 +0100

    Update: Event handling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import IntroComp from './components/verdac/IntroComp.vue'
 import Compositon from './components/verdac/CompositionComp.vue'
+import EventComp from './components/verdac/EventsComp.vue'
+
+const sayHurray = (event: Event) => {
+  // count.value += 5
+  console.log(event)
+}
 </script>
 
 <template>
   <IntroComp />
   <Compositon></Compositon>
+  <EventComp @kick="sayHurray($event)"></EventComp>
 </template>
 
 <style></style>
---
+++ src/components/verdac/EventsComp.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+// Event handling with Native Elements
+// Event handling with Custom Elements (Components)
+
+const eventTrigger = defineEmits(['throw', 'kick']) //Define event bus
+const sayHurray = (event: Event) => {
+  const element = event.target as HTMLButtonElement
+
+  eventTrigger('kick', element)
+}
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <button @click="sayHurray($event)">Trigger events {{ count }}</button>
+    <form action=""></form>
+  </div>
+</template>
